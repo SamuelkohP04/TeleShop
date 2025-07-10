6729ddc74a3496d8e316eb7fdd82e73b73978281
Sam <samuelkoh17@gmail.com>
2025-07-10 23:13:57 +0800

    fix: changed mapping for products for each product

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 import Card from './Components/Card/Card.tsx'
 import Cart from './Components/Cart/Cart'
 const { getData } = require("./db/db");
-const foods = getData();
+const products = getData();
 const tele = window.Telegram.WebApp
 
 function App() {
@@ -14,44 +14,44 @@ function App() {
     tele.ready();
   })
 
-  const onAdd = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist) { // find food if in cart, compare id and add quantity
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
-        )
-      );
+  // const onAdd = (food) => {
+  //   const exist = cartItems.find((x) => x.id === food.id);
+  //   if (exist) { // find food if in cart, compare id and add quantity
+  //     setCartItems(
+  //       cartItems.map((x) =>
+  //         x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+  //       )
+  //     );
 
-    } else { // not in cart, set cartItems to previous state and set quantity to 1
-      setCartItems([...cartItems, { ...food, quantity: 1 }]);
-    }
-  };
+  //   } else { // not in cart, set cartItems to previous state and set quantity to 1
+  //     setCartItems([...cartItems, { ...food, quantity: 1 }]);
+  //   }
+  // };
 
-  const onRemove = (food) => {
-    const exist = cartItems.find((x) => x.id === food.id);
-    if (exist.quantity === 1) {
-      setCartItems(cartItems.filter((x) => x.id !== food.id));
-    } else {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
-        )
-      );
-    }
-  };
+  // const onRemove = (food) => {
+  //   const exist = cartItems.find((x) => x.id === food.id);
+  //   if (exist.quantity === 1) {
+  //     setCartItems(cartItems.filter((x) => x.id !== food.id));
+  //   } else {
+  //     setCartItems(
+  //       cartItems.map((x) =>
+  //         x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+  //       )
+  //     );
+  //   }
+  // };
 
-  const onCheckout = () => {
-    tele.MainButton.text = "Pay :)";
-    tele.MainButton.show();
-  }
+  // const onCheckout = () => {
+  //   tele.MainButton.text = "Pay :)";
+  //   tele.MainButton.show();
+  // }
 
   return (
     <>
-    <h1 className='heading'>Order Food</h1>
-    <Cart cartItems={cartItems} onCheckout={onCheckout}/>
+    <h1 className='heading'>Book a Tarot Card Consultation</h1>
+    {/* <Cart cartItems={cartItems} onCheckout={onCheckout}/> */}
     <div className='cards__container'>
-      {foods.map(food => { return <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove}/>})}
+      {products.map(product => { return <Card product={product} key={product.id} />})}
     </div>
     
     </>
