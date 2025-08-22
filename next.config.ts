8dc538b4d9b81a64d033d07f6cd4050293f04569
SamuelKohP04 <samuelkoh17@gmail.com>
2025-08-22 15:54:29 +0800

    fix: touched up missing files (open next config file & cloudflare run commands)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export", // enables static exports - REMOVED to support API routes
+  serverExternalPackages: [ "@prisma/client", ".prisma/client", "postgres", "jose" ],
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
